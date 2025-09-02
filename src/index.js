0f8547835225f9559cc42058534249a6d3ec5091
Nazima15 <your_email@example.com>
2025-09-02 10:39:42 +0900

    react.js 첫 번째 컴밋

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Hello from './ch03/Hello'; 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Hello toWhat="World!" />
+      <Hello toWhat="props-toWhat 연습!" />
+      <Hello toWhat="aaa" />
+    </div>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
